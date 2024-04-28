d62469946fcd1a820e3133f09d6465749ff789cf
Prabhat Kumar <pkkrsingh877@gmail.com>
2024-04-28 10:59:07 +0530

    Created user folder with some html files to display profile, login user, signup user, update profile and updated overall the way forms look

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-app.debug = True
+app.debug=True
 
 recipes = [
     {
@@ -57,3 +57,25 @@ def create_recipe():
         # Here you can add the received recipe data to your recipes list or database
 
         return "<div>Data Successfully Entered</div>"
+
+@app.route("/recipes/update", methods=["GET","PATCH"])
+def update_recipe():
+    if request.method == "GET":
+        return render_template("recipes/update_recipe.html")
+    elif request.method == "POST":
+        title = request.form.get("title")
+        description = request.form.get("description")
+        ingredients = request.form.get("ingredients")
+        preparation_time = request.form.get("preparation_time")
+        cooking_time = request.form.get("cooking_time")
+        servings = request.form.get("servings")
+
+        # Print or process the received data as needed
+        print(f"Received Updated data: {title}, {description}, {ingredients}, {preparation_time}, {cooking_time}, {servings}")
+
+        # Here you can add the received recipe data to your recipes list or database
+
+        return "<div>Data Successfully Updated</div>"
+
+if __name__ == "__main__":
+    app.run()
